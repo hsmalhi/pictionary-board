619420a3ed653de55c7429669c43153c7f081d63
mrchenliang <c.liang@ualberta.ca>
2019-10-04 15:11:34 -0400

    fixing guessboard

--- src/components/guessboard/Game.tsx
+++ src/components/guessboard/Game.tsx
@@ -16,66 +16,26 @@ const CentreWrapper = styled.div`
   font-family: "Bubblegum Sans", sans-serif;
 `;
 
-const Word = styled(({ word, ...props }) => <div {...props}>{word}</div>)`
-  font-size: 5em;
-  line-height: 1em;
-  padding: 0;
-  margin: 0;
-  font-family: "Bubblegum Sans", sans-serif;
-`;
-
-const Results = styled(({ score, attempts, ...props }) => (
-  <div {...props}>
-    Score: <span>{score}</span>, Attempts: <span>{attempts}</span>
-  </div>
-))`
-  display: block;
-  font-size: 1.5em;
-  span {
-    font-weight: bold;
-  }
-`;
-
-const shuffle = (arr: any) => [...arr].sort(() => 0.5 - Math.random());
-
 export default (props: any) => {
-  const [words, setWords] = useState(() => shuffle(props.words));
-  const [score, setScore] = useState(0);
-  const [attempts, setAttempts] = useState(props.attempts);
-
-  const nextWord = () => {
-    setWords(words.length > 1 ? words.slice(1) : shuffle(props.words));
-    setAttempts(props.attempts);
-  };
 
   const onCorrect = () => {
-    setScore(score + 1);
-    nextWord();
+    
   };
 
   const onIncorrect = () => {
-    if (attempts >= 0) {
-      setAttempts(attempts + 1);
-      return;
-    }
-
-    setScore(Math.max(0, score - 1));
-    nextWord();
+    // setScore(Math.max(0, score - 1));
   };
 
-  const [{ emoji, name }] = words;
 
-  useEffect(() => props.cheatMode && console.log(name), [name]);
+  useEffect(() => props.cheatMode);
 
   return (
     <CentreWrapper>
-      <Word word={emoji} />
       <AnswerBox
-        answer={name}
+        answer={props.word}
         onCorrect={onCorrect}
         onIncorrect={onIncorrect}
       />
-      <Results score={score} attempts={attempts} />
     </CentreWrapper>
   );
 };
