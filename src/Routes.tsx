2899e22fac7b9652e99fc447a6cdd566ebb75e54
mrchenliang <c.liang@ualberta.ca>
2019-10-07 15:26:26 -0400

    adding fixes to the styles on the guess apge

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -20,14 +20,14 @@ const Routes: React.FC = () => {
         <Route exact path="/guess" component={(props : any)=><GuessBoard word={"star wars"} onCorrect={console.log("correct")}/>} />
         <Route exact path="/lobby" component={(props : any)=><LobbySetup/>} />
         <Route exact path="/result" component={(props : any)=><Result/>} />
-        <Route exact path="/display/:id" component={(props : any)=><LeftRightDisplay word="star wars" time={45} side="lef t" socket={socket}/>} />
+        <Route exact path="/display/:id" component={(props : any)=><LeftRightDisplay word="star wars" time={45} side="left" socket={socket}/>} />
         <Route exact path="/game/:id" component={(props : any)=><DrawingDisplay id={5} word="star" time={45} side="left" socket={socket}/>} />
 
-        {/* <Route
+        <Route
           exact
           path="/:id"
           component={(props: any) => <Game {...props} socket={socket} />}
-        /> */}
+        />
         <Route
           exact
           path="/"
